456a9d37e8ae02cd81affeb92e3be74c6a27530e
rafaelwinter01 <rafaelwinter01@gmail.com>
2024-05-21 16:23:40 -0700

    TypeScript corrections

--- src/lib/db/connect.ts
+++ src/lib/db/connect.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
 export async function connect() {
-  const site = process.env.DB_SITE; 
+  const site = process.env.DB_SITE;
 
   try {
+    if (!site) return;
     const connection = await mongoose.connect(site);
   } catch (err) {
     console.log(err);
